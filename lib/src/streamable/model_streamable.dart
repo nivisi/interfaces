351d1322b00368338473341451942908d2fd1f59
Nikita Sirovskiy <nicksirovsky@gmail.com>
2022-10-21 23:12:32 +0300

    Fix typos

--- lib/src/streamable/model_streamable.dart
+++ lib/src/streamable/model_streamable.dart
@@ -1,4 +1,4 @@
 /// Simple abstract class that adds a stream of type `T` named [modelStream].
-abstract class ModelStremable<T> {
+abstract class ModelStreamable<T> {
   Stream<T> get modelStream;
 }
--- lib/src/streamable/state_streamable.dart
+++ lib/src/streamable/state_streamable.dart
@@ -1,4 +1,4 @@
 /// Simple abstract class that adds a stream of type `T` named [stateStream].
-abstract class StateStremable<T> {
+abstract class StateStreamable<T> {
   Stream<T> get stateStream;
 }
